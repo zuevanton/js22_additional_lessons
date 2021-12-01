40e816694d8462271b015e58136b82a2a89fefa7
Anton Zuev <wnfgnqpw@gmail.com>
2021-12-01 22:16:33 +0300

    added lesson03

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-let num = 266219,
-    str = num + '',
-    result = 1;
+'use strict';
 
-for(let n of str) {
-  result *= n;
+let lang = 'ru',
+    ruWeek = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+    enWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+if (lang === 'ru'){
+  ruWeek.forEach(element => console.log(element));
 }
-console.log(`умножение всех чисел в числе ${num} = ${result}`);
-console.log((result ** 3 + '').substr(0, 2));
+else enWeek.forEach(element => console.log(element));
+
+switch(lang) {
+  case 'ru':
+    ruWeek.forEach(element => console.log(element));
+    break;
+  case 'en':
+    enWeek.forEach(element => console.log(element));
+}
+
+let week = {
+  ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+  en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+};
+week[lang].forEach(element => console.log(element));
+
+
+let namePerson = 'А',
+    result = namePerson === 'Артем' ? console.log('Директор') : namePerson === 'А' ? console.log('преподаватель') : console.log('студент');
