ad5bf5e6b8e0c222e81a25d63d19e9b0af7a4741
Anton Zuev <wnfgnqpw@gmail.com>
2021-12-04 12:03:09 +0300

    added lesson04

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
 'use strict';
 
-let lang = 'ru',
-    ruWeek = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-    enWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
-if (lang === 'ru'){
-  ruWeek.forEach(element => console.log(element));
-}
-else enWeek.forEach(element => console.log(element));
-
-switch(lang) {
-  case 'ru':
-    ruWeek.forEach(element => console.log(element));
-    break;
-  case 'en':
-    enWeek.forEach(element => console.log(element));
+const func = (arg) => {
+  if (typeof arg != 'string'){
+    return 'передана не строка'
+  }
+  arg = arg.trim()
+  if (arg.length > 30) {
+    arg = arg.slice(0, 30);
+    return arg + '...';
+  }
+  return arg
 }
 
-let week = {
-  ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-  en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-};
-week[lang].forEach(element => console.log(element));
-
-
-let namePerson = 'Александр',
-    result = namePerson === 'Артем' ? 'Директор' : namePerson === 'Александр' ? 'преподаватель' : 'студент';
-console.log(result);
+console.log(func(1))
+console.log(func('   asdasdasdjknasdjkjnkasdkjnasdkjnasdnjkasdnkjasknjadsknjasdknj    '));
