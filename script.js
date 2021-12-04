2d9a668e98f3cfb793c2997604dfbf64c726ab49
Anton Zuev <wnfgnqpw@gmail.com>
2021-12-04 23:30:04 +0300

    added lesson05

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 'use strict';
+// 1 задание
+let arr = ['876', '2987', '9123', '4987', '24375', '52947', '78123'];
+arr.forEach(function(el){
+  if(el.slice(0, 1) === '2' || el.slice(0, 1) === '4'){
+    console.log(el);
+  }
+});
 
-const func = (arg) => {
-  if (typeof arg != 'string'){
-    return 'передана не строка'
+// 2 задание
+let k = 2;
+for(let i = 2; i < 100; i++){
+  for(k = 2; k <= i; k++){
+    if (i % k === 0){
+      break;
+    }
   }
-  arg = arg.trim()
-  if (arg.length > 30) {
-    arg = arg.slice(0, 30);
-    return arg + '...';
+  if (k === i){
+    console.log('делитель числа ' + i + ': 1 и ' + i);
   }
-  return arg
 }
-
-console.log(func(1))
-console.log(func('   asdasdasdjknasdjkjnkasdkjnasdkjnasdnjkasdnkjasknjadsknjasdknj    '));
