5793d17c911717e086b1bd3222396f2e85d4809c
Anton Zuev <wnfgnqpw@gmail.com>
2021-12-05 15:37:16 +0300

    added lesson07

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 'use strict';
-// 1 задание
-let arr = ['876', '2987', '9123', '4987', '24375', '52947', '78123'];
-arr.forEach(function(el){
-  if(el.slice(0, 1) === '2' || el.slice(0, 1) === '4'){
-    console.log(el);
-  }
-});
 
-// 2 задание
-let k = 2;
-for(let i = 2; i < 100; i++){
-  for(k = 2; k <= i; k++){
-    if (i % k === 0){
-      break;
-    }
+const week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+      div = document.createElement('div'),
+      date = new Date();
+
+function createDay(day, num){
+  // если попадаем на субботу и вскр, то выводим их Ж и К
+  if (num === date.getDay() && (date.getDay() === 6 || date.getDay() === 0)){
+    div.innerHTML += '<p><b><i>' + day + '</i></b></p>';
   }
-  if (k === i){
-    console.log('делитель числа ' + i + ': 1 и ' + i);
+  // если суббота или вскр, то делаем их К
+  else if (num === 0 || num === 6){
+    div.innerHTML += '<p><i>' + day + '</i></p>';
+  }
+  // если попадаем на день, то делаем Ж
+  else if(num === date.getDay()){
+    div.innerHTML += '<p><b>' + day + '</b></p>';
+  }
+  // выводим без всего
+  else {
+    div.innerHTML += '<p>' + day + '</p>';
   }
 }
+
+for(let i = 1; i < week.length; i++){
+  createDay(week[i], i);
+}
+createDay(week[0], 0);
+document.body.append(div);
